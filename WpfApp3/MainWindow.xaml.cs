da629aed2814b03722a9d2fb9a1834b3d7414fa0
CrashTagiyev <121178020+CrashTagiyev@users.noreply.github.com>
2023-06-18 16:42:22 +0300

    Add project files.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        public List<User> Users { get; set; } = new List<User> { };
+        public MainWindow()
+        {
+            try
+            {
+
+                InitializeComponent();
+                this.UsersList.ItemsSource = Users;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (Name_tbox.Text == "" || Surname_tbox.Text == "") throw new Exception("Fill the boxes");
+                UsersList.ItemsSource = null;
+                Users.Add(new User(Name_tbox.Text, Surname_tbox.Text));
+                UsersList.ItemsSource = Users;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ WpfApp3/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+   public static class IDgenerator
+    {
+        public static int Sid { get; set; } = 0;
+    } 
+    public class User
+    {
+        public User( string? name, string? surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public int ID { get; set; } = IDgenerator.Sid++;
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+    }
+}
